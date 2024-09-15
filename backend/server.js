127fb88dbd20ebefbfbf2dfe9c9112f4d959b90a
muhammedrishadpe <rishadpe@gmail.com>
2024-09-15 17:40:22 +0200

    Add CORS support and basic GET route

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+
+app.get('/', (re,res) => {
+return res.json('from backend Side');
+});
+
+app.listen(8081, ()=> {
+    console.log("Listening..")
+})
