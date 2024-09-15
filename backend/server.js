c867348fddb94163c416f34ef085e9427e38c8de
muhammedrishadpe <rishadpe@gmail.com>
2024-09-15 17:55:32 +0200

    Add MySQL database connection and /db route

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,24 @@ const app = express();
 
 app.use(cors())
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"",
+    database:"mysql"
+});
+
 app.get('/', (re,res) => {
 return res.json('from backend Side');
 });
+
+app.get('/db', (req, res) => {
+    const sql = "SELECT * FROM db";
+    db.query(sql, (err,data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
 
 app.listen(8081, ()=> {
     console.log("Listening..")
